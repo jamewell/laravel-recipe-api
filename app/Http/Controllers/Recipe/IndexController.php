8452eec220d5fb1b0661ab304a2adba0dc2d7bc1
jamewell <jamiew746@gmail.com>
2025-06-22 19:30:41 +0200

    feat(recipe): add filtering support to recipe index
    - Implement FilterAction for recipe filtering logic
    - Update IndexController to use filtering

--- app/Http/Controllers/Recipe/IndexController.php
+++ app/Http/Controllers/Recipe/IndexController.php
@@ -2,18 +2,44 @@
 
 namespace App\Http\Controllers\Recipe;
 
+use App\Actions\Recipe\FilterAction;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class IndexController extends Controller
 {
-    public function __invoke(): AnonymousResourceCollection
+    public function __construct(
+        private readonly FilterAction $filterAction
+    ) {}
+
+    public function __invoke(Request $request): AnonymousResourceCollection
     {
-        $recipes = Recipe::with(['kitchen', 'ingredients', 'instructions'])
-            ->latest()
-            ->paginate(10);
+        $query = Recipe::with([
+            'user',
+            'kitchen',
+            'ingredients',
+            'instructions',
+        ]);
+
+        $filteredRecipes = $this->filterAction->execute(
+            $query,
+            $request->only([
+                'kitchen',
+                'servings',
+                'min_servings',
+                'max_servings',
+                'prep_time',
+                'max_prep_time',
+                'cook_time',
+                'max_cook_time',
+                'title',
+            ])
+        );
+
+        $recipes = $filteredRecipes->latest()->paginate(10);
 
         return RecipeResource::collection($recipes);
     }
